a76594a93eec59e8e84a4943ff01c98a48847e16
LLLLKKKK <kan.liu.229@gmail.com>
2014-05-07 21:42:24 +0800

    convert.py

--- _posts/convert.py
+++ _posts/convert.py
@@ -1,24 +1,25 @@
+import sys
 
-filename = '2014-05-07--stdfunction-.md'
-s = None
-with open('test.md', 'r') as f:
-    s = f.read()
-    s = s.replace('[color=red][b]', '##').replace('[/color][/b]', '')
-    s = s.replace('[b][color=red]', '##').replace('[/b][/color]', '')
-
-    lastend = 0
-    while lastend < len(s):
-        begin = s.find('[code]', lastend)
-        if begin == -1:
-            begin = len(s)
-        s = s[:lastend] + s[lastend:begin].replace('_', '\\_').replace('<', '&lt;').replace('>', '&gt;') + s[begin:] # replace('=', '\\=')
-        lastend = s.find('[/code]', begin)
-        if lastend == -1:
-            lastend = len(s)
-        else:
-            lastend += 5
-
-    s = s.replace('[code]', '{% highlight cpp %}').replace('[/code]', '{% endhighlight %}')
-
-with open(filename, 'w') as f:
-    f.write(s)
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    s = None
+    with open('test.md', 'r') as f:
+        s = f.read()
+        s = s.replace('[color=red][b]', '##').replace('[/color][/b]', '')
+        s = s.replace('[b][color=red]', '##').replace('[/b][/color]', '')
+        lastend = 0
+        while lastend < len(s):
+            begin = s.find('[code]', lastend)
+            if begin == -1:
+                begin = len(s)
+            s = s[:lastend] + s[lastend:begin].replace('_', '\\_').replace('<', '&lt;').replace('>', '&gt;').replace['[', '\[').replace(']', '\]') + s[begin:] # replace('=', '\\=')
+            lastend = s.find('[/code]', begin)
+            if lastend == -1:
+                lastend = len(s)
+            else:
+                lastend += len('[code]')
+            
+        s = s.replace('[code]', '{% highlight cpp %}').replace('[/code]', '{% endhighlight %}')
+        
+    with open(filename, 'w') as f:
+        f.write(s)
